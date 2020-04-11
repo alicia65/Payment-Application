99994acbaeacde2e78ad20492dc059626ea300c6
alicia65 <alicia65>
2020-04-11 17:36:06 -0500

    Add project files.

---
+++ Payment Application/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Payment_Application
+{
+    partial class frmCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboNames = new System.Windows.Forms.ComboBox();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblPaymentMethod = new System.Windows.Forms.Label();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelectPayment = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblPayment = new System.Windows.Forms.Label();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboNames
+            // 
+            this.cboNames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboNames.FormattingEnabled = true;
+            this.cboNames.Location = new System.Drawing.Point(203, 34);
+            this.cboNames.Name = "cboNames";
+            this.cboNames.Size = new System.Drawing.Size(230, 24);
+            this.cboNames.TabIndex = 1;
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.Location = new System.Drawing.Point(34, 37);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(123, 23);
+            this.lblCustomerName.TabIndex = 2;
+            this.lblCustomerName.Text = "Customer name:";
+            // 
+            // lblPaymentMethod
+            // 
+            this.lblPaymentMethod.Location = new System.Drawing.Point(34, 83);
+            this.lblPaymentMethod.Name = "lblPaymentMethod";
+            this.lblPaymentMethod.Size = new System.Drawing.Size(123, 23);
+            this.lblPaymentMethod.TabIndex = 4;
+            this.lblPaymentMethod.Text = "Payment method:";
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.lblPayment);
+            this.GroupBox1.Location = new System.Drawing.Point(37, 118);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(248, 82);
+            this.GroupBox1.TabIndex = 5;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Change to credit card.";
+            // 
+            // btnSelectPayment
+            // 
+            this.btnSelectPayment.Location = new System.Drawing.Point(357, 127);
+            this.btnSelectPayment.Name = "btnSelectPayment";
+            this.btnSelectPayment.Size = new System.Drawing.Size(123, 23);
+            this.btnSelectPayment.TabIndex = 6;
+            this.btnSelectPayment.Text = "Select Payment";
+            this.btnSelectPayment.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(203, 224);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(127, 23);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(357, 223);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(129, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // lblPayment
+            // 
+            this.lblPayment.AutoEllipsis = true;
+            this.lblPayment.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPayment.Location = new System.Drawing.Point(0, 18);
+            this.lblPayment.Name = "lblPayment";
+            this.lblPayment.Size = new System.Drawing.Size(248, 61);
+            this.lblPayment.TabIndex = 0;
+            this.lblPayment.Text = "\"\"";
+            // 
+            // frmCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 272);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSelectPayment);
+            this.Controls.Add(this.GroupBox1);
+            this.Controls.Add(this.lblPaymentMethod);
+            this.Controls.Add(this.lblCustomerName);
+            this.Controls.Add(this.cboNames);
+            this.Name = "frmCustomer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer";
+            this.GroupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboNames;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Label lblPaymentMethod;
+        private System.Windows.Forms.GroupBox GroupBox1;
+        private System.Windows.Forms.Label lblPayment;
+        private System.Windows.Forms.Button btnSelectPayment;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Payment Application/frmPayment.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Payment_Application
+{
+    partial class frmPayment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdoCreditCard = new System.Windows.Forms.RadioButton();
+            this.rdoBillCustomer = new System.Windows.Forms.RadioButton();
+            this.lblCreditCardType = new System.Windows.Forms.Label();
+            this.CreditCardType = new System.Windows.Forms.ListBox();
+            this.lblCardNumber = new System.Windows.Forms.Label();
+            this.txtCardNumber = new System.Windows.Forms.TextBox();
+            this.lblExpirationDate = new System.Windows.Forms.Label();
+            this.cboExpirationMonth = new System.Windows.Forms.ComboBox();
+            this.cboExpirationYear = new System.Windows.Forms.ComboBox();
+            this.chkDefault = new System.Windows.Forms.CheckBox();
+            this.bntOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoBillCustomer);
+            this.groupBox1.Controls.Add(this.rdoCreditCard);
+            this.groupBox1.Location = new System.Drawing.Point(32, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Billing";
+            // 
+            // rdoCreditCard
+            // 
+            this.rdoCreditCard.AutoSize = true;
+            this.rdoCreditCard.Checked = true;
+            this.rdoCreditCard.Location = new System.Drawing.Point(38, 45);
+            this.rdoCreditCard.Name = "rdoCreditCard";
+            this.rdoCreditCard.Size = new System.Drawing.Size(100, 21);
+            this.rdoCreditCard.TabIndex = 0;
+            this.rdoCreditCard.TabStop = true;
+            this.rdoCreditCard.Text = "Credit Card";
+            this.rdoCreditCard.UseVisualStyleBackColor = true;
+            // 
+            // rdoBillCustomer
+            // 
+            this.rdoBillCustomer.AutoSize = true;
+            this.rdoBillCustomer.Location = new System.Drawing.Point(269, 45);
+            this.rdoBillCustomer.Name = "rdoBillCustomer";
+            this.rdoBillCustomer.Size = new System.Drawing.Size(111, 21);
+            this.rdoBillCustomer.TabIndex = 1;
+            this.rdoBillCustomer.Text = "Bill Customer";
+            this.rdoBillCustomer.UseVisualStyleBackColor = true;
+            // 
+            // lblCreditCardType
+            // 
+            this.lblCreditCardType.Location = new System.Drawing.Point(32, 171);
+            this.lblCreditCardType.Name = "lblCreditCardType";
+            this.lblCreditCardType.Size = new System.Drawing.Size(138, 23);
+            this.lblCreditCardType.TabIndex = 10;
+            this.lblCreditCardType.Text = "Credit Card Type:";
+            // 
+            // CreditCardType
+            // 
+            this.CreditCardType.FormattingEnabled = true;
+            this.CreditCardType.ItemHeight = 16;
+            this.CreditCardType.Location = new System.Drawing.Point(199, 171);
+            this.CreditCardType.Name = "CreditCardType";
+            this.CreditCardType.Size = new System.Drawing.Size(293, 84);
+            this.CreditCardType.TabIndex = 0;
+            // 
+            // lblCardNumber
+            // 
+            this.lblCardNumber.Location = new System.Drawing.Point(32, 280);
+            this.lblCardNumber.Name = "lblCardNumber";
+            this.lblCardNumber.Size = new System.Drawing.Size(94, 23);
+            this.lblCardNumber.TabIndex = 8;
+            this.lblCardNumber.Text = "Card number:\r\n";
+            // 
+            // txtCardNumber
+            // 
+            this.txtCardNumber.Location = new System.Drawing.Point(199, 280);
+            this.txtCardNumber.Name = "txtCardNumber";
+            this.txtCardNumber.Size = new System.Drawing.Size(293, 22);
+            this.txtCardNumber.TabIndex = 1;
+            // 
+            // lblExpirationDate
+            // 
+            this.lblExpirationDate.Location = new System.Drawing.Point(35, 331);
+            this.lblExpirationDate.Name = "lblExpirationDate";
+            this.lblExpirationDate.Size = new System.Drawing.Size(117, 23);
+            this.lblExpirationDate.TabIndex = 6;
+            this.lblExpirationDate.Text = "Expiration Date:";
+            // 
+            // cboExpirationMonth
+            // 
+            this.cboExpirationMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboExpirationMonth.FormattingEnabled = true;
+            this.cboExpirationMonth.Location = new System.Drawing.Point(199, 328);
+            this.cboExpirationMonth.Name = "cboExpirationMonth";
+            this.cboExpirationMonth.Size = new System.Drawing.Size(121, 24);
+            this.cboExpirationMonth.TabIndex = 2;
+            // 
+            // cboExpirationYear
+            // 
+            this.cboExpirationYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboExpirationYear.FormattingEnabled = true;
+            this.cboExpirationYear.Location = new System.Drawing.Point(371, 328);
+            this.cboExpirationYear.Name = "cboExpirationYear";
+            this.cboExpirationYear.Size = new System.Drawing.Size(121, 24);
+            this.cboExpirationYear.TabIndex = 3;
+            // 
+            // chkDefault
+            // 
+            this.chkDefault.AutoSize = true;
+            this.chkDefault.Checked = true;
+            this.chkDefault.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDefault.Location = new System.Drawing.Point(38, 385);
+            this.chkDefault.Name = "chkDefault";
+            this.chkDefault.Size = new System.Drawing.Size(208, 21);
+            this.chkDefault.TabIndex = 4;
+            this.chkDefault.Text = "Set as default billing method";
+            this.chkDefault.UseVisualStyleBackColor = true;
+            // 
+            // bntOk
+            // 
+            this.bntOk.Location = new System.Drawing.Point(264, 415);
+            this.bntOk.Name = "bntOk";
+            this.bntOk.Size = new System.Drawing.Size(75, 23);
+            this.bntOk.TabIndex = 5;
+            this.bntOk.Text = "Ok";
+            this.bntOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(404, 415);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 6;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmPayment
+            // 
+            this.AcceptButton = this.bntOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.bntOk);
+            this.Controls.Add(this.chkDefault);
+            this.Controls.Add(this.cboExpirationYear);
+            this.Controls.Add(this.cboExpirationMonth);
+            this.Controls.Add(this.lblExpirationDate);
+            this.Controls.Add(this.txtCardNumber);
+            this.Controls.Add(this.lblCardNumber);
+            this.Controls.Add(this.CreditCardType);
+            this.Controls.Add(this.lblCreditCardType);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmPayment";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Payment";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoBillCustomer;
+        private System.Windows.Forms.RadioButton rdoCreditCard;
+        private System.Windows.Forms.Label lblCreditCardType;
+        private System.Windows.Forms.ListBox CreditCardType;
+        private System.Windows.Forms.Label lblCardNumber;
+        private System.Windows.Forms.TextBox txtCardNumber;
+        private System.Windows.Forms.Label lblExpirationDate;
+        private System.Windows.Forms.ComboBox cboExpirationMonth;
+        private System.Windows.Forms.ComboBox cboExpirationYear;
+        private System.Windows.Forms.CheckBox chkDefault;
+        private System.Windows.Forms.Button bntOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
